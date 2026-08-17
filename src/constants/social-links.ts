67557ee9676d1fecec9d36ae790fc516b7a91ec3
Juan Carlos Avila <juanvs23@gmail.com>
2026-08-17 17:23:14 -0400

    fix: update X link to x.com

--- src/constants/social-links.ts
+++ src/constants/social-links.ts
@@ -7,6 +7,6 @@ export interface SocialLink {
 export const socialLinks: SocialLink[] = [
   { name: 'GitHub', href: 'https://github.com/juanvs23', icon: 'github' },
   { name: 'LinkedIn', href: 'https://www.linkedin.com/in/juanvs23/', icon: 'linkedin' },
-  { name: 'X', href: 'https://twitter.com/juanvs23', icon: 'twitter' },
+  { name: 'X', href: 'https://x.com/juanvs23', icon: 'twitter' },
   { name: 'Facebook', href: 'https://www.facebook.com/juancarlos.avila.1420/', icon: 'facebook' },
 ];
